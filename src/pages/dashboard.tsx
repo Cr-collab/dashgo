b0dbd0a08b505c9e1ec0effde30135495d3c7e35
Cristiano Azevdo <cristiccorrea@gmail.com>
2022-01-05 17:09:17 -0300

    Finalizando Sidebar

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,7 +1,26 @@
+import { Flex } from "@chakra-ui/react";
 import { Header } from "../components/Header";
+import { Sidebar } from "../components/Sidebar";
 
 export default function Dasboard(){
   return(
-    <Header/>
+    <Flex 
+    direction="column"
+    h="100vh"
+    >
+          <Header/>
+
+          <Flex
+          
+          w="100%"
+          my="6"
+          maxWidth={1480}
+          mx="auto"
+          px="6"
+          >
+            <Sidebar/>
+          </Flex>
+
+    </Flex>
     )
 }

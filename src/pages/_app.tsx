10de82aa5407f9cd69427efed20729af340d8c0b
Cristiano Azevdo <cristiccorrea@gmail.com>
2022-01-13 18:37:40 -0300

    configurando react query e entendendo ele

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,20 +3,29 @@ import { ChakraProvider } from '@chakra-ui/react'
 import { theme } from '../styles/theme'
 import { SidebarDrawerProvider } from '../contexts/SidebarDrawerContent'
 import { makeServer } from '../services/mirage'
+import {QueryClient, QueryClientProvider} from 'react-query'
+import { ReactQueryDevtools } from 'react-query/devtools'
 
 if(process.env.NODE_ENV === 'development'){
   makeServer()
 }
 
 function MyApp({ Component, pageProps }: AppProps) {
+ 
+  const queryClient = new QueryClient()
 
   return (
+    <QueryClientProvider client={queryClient}>
+
     <ChakraProvider 
      theme={theme}>
        <SidebarDrawerProvider>
           <Component {...pageProps}  />
        </SidebarDrawerProvider>
     </ChakraProvider>
+
+    <ReactQueryDevtools/>
+    </QueryClientProvider>
   )
 }
 
